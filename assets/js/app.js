70a10f6f2e1aa69002761d477bd4257be637b735
masontotall <68935404+masontotall@users.noreply.github.com>
2021-06-11 13:54:52 -0500

    Created space to hold chart on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
 // @TODO: YOUR CODE HERE!
 
 // Create the chart
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create SVG wrapper and group to hold chart and set margins
+
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import and Format data
+
+
